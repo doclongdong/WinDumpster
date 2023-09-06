d2ee90898e080586d532c020d7b42e48741b395c
Chungus McGee <106646061+chungus-mcgee@users.noreply.github.com>
2023-09-06 16:53:38 -0700

    include greyscale in ScreenTime

--- ScreenTime/OutsideHoursCountDown.ps1
+++ ScreenTime/OutsideHoursCountDown.ps1
@@ -1,3 +1,5 @@
+# Note for the greyscale to work, you need AutoHotkeyV2 installed
+
 $i = 600
 
 $min = Get-Date '08:00'
@@ -5,6 +7,9 @@ $max = Get-Date '16:01'
 $now = Get-Date
 
 if ($now.TimeOfDay -le $min.TimeOfDay -or $now.TimeOfDay -ge $max.TimeOfDay) {
+    $header = Split-Path -Parent $PSCommandPath
+    $fullPath = Join-Path -Path $header -ChildPath "GreyScale.ahk"
+    PowerShell $fullPath
 	do {
 		Write-Host "Outside hours: Seconds to logout" $i
 	        Sleep 1

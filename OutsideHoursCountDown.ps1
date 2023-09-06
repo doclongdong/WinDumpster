c28c051662e3486b209d354b18d017f5599cab22
Chungus McGee <106646061+chungus-mcgee@users.noreply.github.com>
2023-09-06 16:15:45 -0700

    update hours checking to be conditional

--- OutsideHoursCountDown.ps1
+++ OutsideHoursCountDown.ps1
@@ -1,10 +1,21 @@
-$i = 10
+$i = 600
 
-do {
-    Write-Host "Outside hours: Seconds to logout" $i
-    Sleep 1
-    $i--
-} while ($i -gt 0)
+$min = Get-Date '08:00'
+$max = Get-Date '16:01'
+$now = Get-Date
+
+if ($now.TimeOfDay -le $min.TimeOfDay -or $now.TimeOfDay -ge $max.TimeOfDay) {
+	do {
+		Write-Host "Outside hours: Seconds to logout" $i
+	        Sleep 1
+		$i--
+	} while ($i -gt 0)
 
 Write-Host "Locking screen"
 rundll32.exe user32.dll,LockWorkStation
+
+} else{
+	Write-Host "Usage is allowed"
+}
+
+

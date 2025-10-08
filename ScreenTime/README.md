17f5efbe293899d707699b60c57eb5e716eb2962
Chungus McGee <106646061+chungus-mcgee@users.noreply.github.com>
2025-10-08 12:41:45 -0700

    Update screentime to have lower limits, shutdown after lock, and prevent grayscale problems with toggling by reading from registry

--- ScreenTime/README.md
+++ ScreenTime/README.md
@@ -7,9 +7,12 @@ The screentime tool:
 - Makes the screen grayscale (requires AHKv2)
 - Locks, logouts, or shutdowns the computer after an interval
 
+
+It is configured right now to disallow usage from all hours except 7-9pm. Its current behavior is to first quickly lock the screen, then shutdown after 5 minutes. 
+
 It opens a window in powershell which can be exited by the user.
 
-This can be run in Task Scheduler for automation, eg Action=`powershell -windowstyle=hidden /path/to/ScreenTime`
+This can be run in Task Scheduler for automation, eg Action=`powershell -windowstyle=hidden /path/to/ScreenTime`. Its best used with the "on workstation unlock" plus repeat for every "x" minutes. 
 
 
 NOTE: the color filters for windows should be enabled in the Windows settings in order for the AHK ctrl+win+C shortcut to work for Grayscale.
--- ScreenTime/Screentime.ps1
+++ ScreenTime/Screentime.ps1
@@ -1,29 +1,42 @@
 # Note for the greyscale to work, you need AutoHotkeyV2 installed
 
-$i = 30
+$i = 10
 
-$min = Get-Date '18:00'
-$max = Get-Date '23:30'
+# this very narrow scheduled time for the user is between 7pm-9pm, outside of that the 
+# computer will screentime-lock+shutdown
+$min = Get-Date '19:00'
+$max = Get-Date '21:00'
 $now = Get-Date
 
 if (($now.TimeOfDay -le $min.TimeOfDay) -or ($now.TimeOfDay -ge $max.TimeOfDay)) {
-    Sleep 5
+    # Sleep 5
     $header = Split-Path -Parent $PSCommandPath
     $fullPath = Join-Path -Path $header -ChildPath "GreyScale.ahk"
-    PowerShell $fullPath
-	do {
-		Write-Host "Outside hours: Seconds to logout" $i
-	        Sleep 1
-		$i--
-	} while ($i -gt 0)
 
-# Write-Host "Locking screen"
-# rundll32.exe user32.dll,LockWorkStation
+    # only trigger grayscale if needed
+    $grayScaleOn = Get-ItemPropertyValue 'Registry::\HKEY_CURRENT_USER\Software\Microsoft\ColorFiltering' -Name Active
+    if ($grayScaleOn -eq 0) {
+        PowerShell $fullPath
+    } 
+
+    
+    do {
+        Write-Host "Outside hours: Seconds to logout" $i
+	Sleep 1
+        $i--
+    } while ($i -gt 0)
+
+
+# Immediately lock the screen, then initiate shutdown in 5 minutes.
+Write-Host "Locking screen"
+rundll32.exe user32.dll,LockWorkStation
+
+Write-Host "Waiting 5 minutes before shutdown"
+Sleep 300
 Write-Host "Shutting Down"
-shutdown /s
+Shutdown /s
 # Write-Host "Logging off"
 # logoff
-
 
 } else{
 	Write-Host "Usage is allowed"
